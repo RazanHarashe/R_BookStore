f797054069bdebe41202e7b690756208a9775db4
RazanHarashe <harashe115@gmail.com>
2023-12-12 21:25:14 +0200

    add asyncHandler

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import * as AuthController from "./auth.controller.js";
 import fileUpload, { fileValidation } from "../../services/multer.js";
+import { asyncHandler } from "../../services/errorHandling.js";
 const router = Router();
 
 router.post(
   "/signup",
   fileUpload(fileValidation.image).single("image"),
-  AuthController.signUp
+  asyncHandler(AuthController.signUp)
 );
-router.post("/signin", AuthController.signIn);
-router.get("/confirmEmail/:token", AuthController.confirmEmail);
-router.patch("/sendCode", AuthController.sendCode);
-router.patch("/forgotPassword", AuthController.forgotPassword);
+router.post("/signin", asyncHandler(AuthController.signIn));
+router.get("/confirmEmail/:token", asyncHandler(AuthController.confirmEmail));
+router.patch("/sendCode", asyncHandler(AuthController.sendCode));
+router.patch("/forgotPassword", asyncHandler(AuthController.forgotPassword));
 
 export default router;
