d39112c82ae6637fce3260828a3eb08036486190
RazanHarashe <harashe115@gmail.com>
2023-12-12 03:59:31 +0200

    add signup & Sign in & confirm email

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -1,12 +1,14 @@
 import connectDB from "../../db/connection.js";
-
+import authRouter from "./auth/auth.router.js";
 const initApp = (app, express) => {
   app.use(express.json());
   connectDB();
 
-  app.get("*",(req,res)=>{
-    return res.json({message:"page not found"});
-  })
+  app.use("/auth", authRouter);
+
+  app.get("*", (req, res) => {
+    return res.json({ message: "page not found" });
+  });
 };
 
 export default initApp;
