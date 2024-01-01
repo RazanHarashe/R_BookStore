64e0e4de06aa7aa698ff8423d0d977e10efe981f
RazanHarashe <harashe115@gmail.com>
2024-01-01 20:09:14 +0200

    add cors & vercel file

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -2,10 +2,11 @@ import connectDB from "../../db/connection.js";
 import authRouter from "./auth/auth.router.js";
 import genreRouter from "./genre/genre.router.js";
 import bookRouter from "./books/books.router.js";
+import cors from "cors";
 const initApp = (app, express) => {
   app.use(express.json());
   connectDB();
-
+  app.use(cors());
   app.use("/auth", authRouter);
   app.use("/genre", genreRouter);
   app.use("/books", bookRouter);
